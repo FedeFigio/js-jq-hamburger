05f36c9635957a967adb6a74939c145679e89a36
Federico <federicofigini90@gmail.com>
2021-03-10 15:09:08 +0100

    finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
-var ahamMenu = $("a")
+var aOpenMenu = $(".header-right>a")
+var aCloseMenu = $(".hamburger-menu>a")
 
 
 
-ahamMenu.click(function() {
-    $(".hamburger-menu").toggle();
+aOpenMenu.click(function() {
+    $(".hamburger-menu").show();
+});
+
+aCloseMenu.click(function() {
+    $(".hamburger-menu").hide();
 });
